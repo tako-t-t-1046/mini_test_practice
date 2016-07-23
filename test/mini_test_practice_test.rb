598168f4341e000efad6a8f03f7a4a7a99b78adb
None <tako.t.t.1046@gmail.com>
2016-07-23 07:40:37 +0000

    travis CI test

--- test/mini_test_practice_test.rb
+++ test/mini_test_practice_test.rb
@@ -11,14 +11,14 @@ class MiniTestPracticeTest < Minitest::Test
   end
 
   def test_odd?
-    assert_equal false, @main.odd?(-10000), '2 is not odd'
-    assert_equal false, @main.odd?(-2), '0 is not odd'
-    assert_equal true, @main.odd?(-1), '0 is not odd'
+    assert_equal false, @main.odd?(-10000), '-10000 is not odd'
+    assert_equal false, @main.odd?(-2), '-2 is not odd'
+    assert_equal true, @main.odd?(-1), '-1 is not odd'
     assert_equal false, @main.odd?(0), '0 is not odd'
     assert_equal true, @main.odd?(1), '1 is odd'
     assert_equal false, @main.odd?(2), '2 is not odd'
-    assert_equal false, @main.odd?(10000), '2 is not odd'
-    assert @main.odd?(3), '3 is odd'
+    assert_equal false, @main.odd?(10000), '10000 is not odd'
+    assert_equal true, @main.odd?(3), '3 is odd'
     refute @main.odd?(4), '4 is not odd'
     assert @main.odd?(5), '5 is odd'
     refute @main.odd?(6), '6 is not odd'
