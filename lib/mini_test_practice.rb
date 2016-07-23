bc0a54e0c77c339940ee58e82af8abe77b395b3e
None <tako.t.t.1046@gmail.com>
2016-07-23 07:29:11 +0000

    first commit

---
+++ lib/mini_test_practice.rb
@@ -0,0 +1,13 @@
+require "mini_test_practice/version"
+
+module MiniTestPractice
+  class Main
+    def odd?(n)
+      if n % 2 == 1 
+        return true
+      else
+        return false
+      end
+    end
+  end
+end
---
+++ test/mini_test_practice_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+#require 'mini_test_practice'
+
+class MiniTestPracticeTest < Minitest::Test
+  def setup
+    @main = ::MiniTestPractice::Main.new
+  end
+  
+  def test_that_it_has_a_version_number
+    refute_nil ::MiniTestPractice::VERSION
+  end
+
+  def test_odd?
+    assert_equal false, @main.odd?(-10000), '2 is not odd'
+    assert_equal false, @main.odd?(-2), '0 is not odd'
+    assert_equal true, @main.odd?(-1), '0 is not odd'
+    assert_equal false, @main.odd?(0), '0 is not odd'
+    assert_equal true, @main.odd?(1), '1 is odd'
+    assert_equal false, @main.odd?(2), '2 is not odd'
+    assert_equal false, @main.odd?(10000), '2 is not odd'
+    assert @main.odd?(3), '3 is odd'
+    refute @main.odd?(4), '4 is not odd'
+    assert @main.odd?(5), '5 is odd'
+    refute @main.odd?(6), '6 is not odd'
+    assert @main.odd?(7), '7 is odd'
+  end
+end
